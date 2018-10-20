5ec8bbcc3b9703a7afddb73ef67e80a2302ac97b
MikeGlaw <MikeGlaw@users.noreply.github.com>
2018-10-20 17:53:42 -0400

    Going a little further

--- python.py
+++ python.py
@@ -7,15 +7,21 @@ file.readline()
 for line in file:
     gradeSum = 0
     gradeCount = 0
-    
+
     gradeList = line.rstrip().split(" ")
     studentNum=gradeList[0]
     studentNaam=gradeList[1]
 
     for grade in gradeList[2:]:
-        gradeSum+=int(grade)
-        gradeCount+=1
+        if int(grade)!=0:
+            gradeSum+=int(grade)
+            gradeCount+=1
+        else:
+            continue
 
-    print(studentNaam,"has an average of",gradeSum/gradeCount)
+    if gradeSum!=0:
+        print(studentNaam,"has an average of",gradeSum/gradeCount)
+    else:
+        print(studentNaam,"has an average of 0.")
 
 file.close()
